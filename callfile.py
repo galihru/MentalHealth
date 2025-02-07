da6b0b5c84278232f5354e9f2d78d34bde73d31e
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-07 16:31:49 +0700

    Update Website
    Update Website

---
+++ callfile.py
@@ -0,0 +1,15 @@
+import os
+
+def extract_filenames(folder_path, output_file):
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                relative_path = os.path.relpath(os.path.join(root, file), folder_path)
+                f.write(f'\"\{folder_path}\{relative_path}",\n')
+    print(f"Nama file berhasil disimpan dalam {output_file}")
+
+# Ganti 'path_to_folder' dengan lokasi folder yang ingin Anda ekstrak
+folder_path = ""
+output_file = ""
+
+extract_filenames(folder_path, output_file)
