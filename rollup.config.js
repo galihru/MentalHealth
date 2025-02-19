f8bbd9cf025d07e345e17aef3daf870bd3e7f438
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-20 00:10:53 +0700

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/EmotionDetection.js',
+  output: [
+    {
+      file: 'dist/emotion-detection.js',
+      format: 'umd',
+      name: 'EmotionDetection',
+      sourcemap: true
+    },
+    {
+      file: 'dist/emotion-detection.esm.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser()
+  ]
+};
